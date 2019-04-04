fbd4b1bd9938ecd10a860dc25dea853f591a83df
kelwama <48866080+kelwama@users.noreply.github.com>
2019-04-03 19:41:57 -0500

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,4 +1,10 @@
-function menuIcon(x) {
-    x.classList.toggle("change");
-    document.getElementById("nav-links").classList.toggle("hide");
-  }
+$(".nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+    $("a").removeClass("active");
+    $(this).addClass("active");
+    $("#logo a").removeClass("active");
+});
